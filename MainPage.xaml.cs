4d63f3b910262a2053ba57541243ca9cf35d4abf
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-09-04 22:37:13 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+
+namespace WochentagUwp
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            tblDayOfWeek.Text = dpDate.Date.DayOfWeek.ToString();
+        }
+
+        private void dpDate_DateChanged(object sender, DatePickerValueChangedEventArgs e)
+        {
+            tblDayOfWeek.Text = e.NewDate.DayOfWeek.ToString();
+        }
+    }
+}
